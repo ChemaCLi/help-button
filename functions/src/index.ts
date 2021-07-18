3a4230e579f92254a192e833fe60ed637a5ef43f
Jose Maria CL <chema9703@gmail.com>
2021-07-17 23:23:02 -0500

    Send message to telegram chat via Http API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,36 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions'
+import * as express from 'express'
+import * as cors from 'cors'
+import axios from 'axios'
 
-export const testFunction = functions.https.onRequest(async (req, res) => {
-  res.send({ status: 200 })
+// give us the possibility of manage request properly
+const app = express()
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }))
+
+// our single entry point for every message
+app.get('/:chat_id', async (req, res) => {
+  try {
+    const chat_id = req.params.chat_id
+
+    const result = await axios.post(
+      `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        chat_id,
+        text: `La abuelita necesita ayuda...`
+      }
+    )
+  
+    return res.status(200).send({
+      method: 'sendMessage',
+      chat_id,
+      result: result.data,
+    })
+  } catch (e) {
+    return res.status(500).send({ error: e.message || e })
+  }
 })
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// this is the only function it will be published in firebase
+export const router = functions.https.onRequest(app)
